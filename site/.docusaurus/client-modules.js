7318d49369bd0c2ee64dfc780f334d9f70212d59
glass-panel <63582311+glass-panel@users.noreply.github.com.>
2025-07-08 10:56:16 +0800

    [Fix] rm .docusaurus

--- site/.docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("D:\\Dev\\incubator-teaclave-website\\site\\.docusaurus\\docusaurus-plugin-css-cascade-layers\\default\\layers.css"),
-  require("D:\\Dev\\incubator-teaclave-website\\site\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("D:\\Dev\\incubator-teaclave-website\\site\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("D:\\Dev\\incubator-teaclave-website\\site\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("D:\\Dev\\incubator-teaclave-website\\site\\src\\css\\custom.css"),
-];
